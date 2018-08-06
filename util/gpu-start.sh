1691095dff1263eca128fa66241109cbbe1aaceb
Vincent Ying <vincenthying@gmail.com>
2018-08-05 18:49:48 -0700

    Modify scripts for 2 clusters

--- util/gpu-start.sh
+++ util/gpu-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 peg start gpu-cluster
-peg service gpu-cluster cassandra start
+#peg service gpu-cluster cassandra start
 peg service gpu-cluster zookeeper start
 peg service gpu-cluster hadoop start
 peg service gpu-cluster spark start
