76dfbc6bdc760cf2f387befb86ba2d93ae0537d7
Vincent Ying <vincenthying@gmail.com>
2018-08-05 17:02:13 -0700

    Config files

---
+++ config.py
@@ -0,0 +1,9 @@
+MODEL_DIR = '/home/ubuntu/MLwatch/ml_model'
+IMAGES_DIR= '/home/ubuntu/ILSVRC/Data/DET/test'
+KAFKA_CONFIG = {
+                'topic'  : 'demo',
+                'brokers': 'localhost:9092'
+               }
+KEYSPACE = "inception_v3_top1"
+
+CASS_CLUSTER=['34.218.39.129', '52.37.74.65', '54.71.40.181', '52.27.254.108']
