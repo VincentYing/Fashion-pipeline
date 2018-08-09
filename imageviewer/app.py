13128fb1b9250d6d42fe4906d2fd0a9f6c96898e
Vincent Ying <vincenthying@gmail.com>
2018-08-09 11:39:56 -0700

    Flask Imageviewer added

---
+++ imageviewer/app.py
@@ -0,0 +1,52 @@
+import csv
+from flask import Flask, render_template, request, redirect, url_for
+import requests
+from pager import Pager
+
+
+def read_table(url):
+    """Return a list of dict"""
+    # r = requests.get(url)
+    with open(url) as f:
+        return [row for row in csv.DictReader(f.readlines())]
+
+
+APPNAME = "Fast Fashion Classification"
+STATIC_FOLDER = 'example'
+TABLE_FILE = "example/fakecatalog.csv"
+
+table = read_table(TABLE_FILE)
+pager = Pager(len(table))
+
+
+app = Flask(__name__, static_folder=STATIC_FOLDER)
+app.config.update(
+    APPNAME=APPNAME,
+    )
+
+
+@app.route('/')
+def index():
+    return redirect('/0')
+
+
+@app.route('/<int:ind>/')
+def image_view(ind=None):
+    if ind >= pager.count:
+        return render_template("404.html"), 404
+    else:
+        pager.current = ind
+        return render_template(
+            'imageview.html',
+            index=ind,
+            pager=pager,
+            data=table[ind])
+
+
+@app.route('/goto', methods=['POST', 'GET'])    
+def goto():
+    return redirect('/' + request.form['index'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
