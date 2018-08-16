5832ec9aa0e2f9d1aa54b2182add2c91c4e47143
Vincent Ying <vincenthying@gmail.com>
2018-08-15 20:54:47 -0700

    Dir Reorg and Frontend Cosmetics

--- util/cluster-spark-start.sh
+++ util/cluster-spark-start.sh
@@ -2,5 +2,6 @@
 peg start spark-cluster
 peg service spark-cluster hadoop start
 peg service spark-cluster zookeeper start
+sleep 5s
 peg service spark-cluster spark start
 peg service spark-cluster kafka start
